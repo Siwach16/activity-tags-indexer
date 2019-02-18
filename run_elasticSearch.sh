e20db7432f946a7f43c768433e2eef523bd1dc3c
Ajay Siwach <ajaysiwach16@gmail.com>
2019-02-18 16:54:25 +0530

    Update run_elasticSearch.sh

--- run_elasticSearch.sh
+++ run_elasticSearch.sh
@@ -1,1 +1,1 @@
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e "http.cors.enabled=true" -e "http.cors.allow-origin=*" docker.elastic.co/elasticsearch/elasticsearch:6.5.4
+docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e "http.cors.enabled=true" -e "http.cors.allow-origin=*" docker.elastic.co/elasticsearch/elasticsearch:6.5.4
